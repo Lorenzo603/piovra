fe871eaeb4adad4da9a52ee5091dec00b46963f2
Lorenzo603 <lorenzo.furrer@gmail.com>
2016-11-10 13:37:06 +0100

    error messages

--- piovra/src/main/java/it/lf/piovra/controllers/LoginPageController.java
+++ piovra/src/main/java/it/lf/piovra/controllers/LoginPageController.java
@@ -5,13 +5,12 @@ import it.lf.piovra.facades.UserFacade;
 import it.lf.piovra.facades.data.RegistrationResult;
 import it.lf.piovra.facades.data.RegistrationResultStatus;
 import it.lf.piovra.views.forms.RegisterForm;
-import it.lf.piovra.views.forms.validators.PasswordValidator;
+import it.lf.piovra.views.forms.validators.RegisterFormValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,7 +26,7 @@ public class LoginPageController extends AbstractController {
     private UserFacade userFacade;
 
     @Autowired
-    private PasswordValidator passwordValidator;
+    private RegisterFormValidator registerFormValidator;
 
     @ModelAttribute(value = "registerForm")
     public RegisterForm getRegisterForm() {
@@ -41,7 +40,7 @@ public class LoginPageController extends AbstractController {
 
     @RequestMapping(value = "/doRegister", method = RequestMethod.POST)
     public String doRegister(@Valid RegisterForm registerForm, BindingResult bindingResult) {
-        passwordValidator.validate(registerForm, bindingResult);
+        registerFormValidator.validate(registerForm, bindingResult);
         if (bindingResult.hasErrors()) {
             return ControllerConstants.Views.LOGIN_VIEW;
         }
