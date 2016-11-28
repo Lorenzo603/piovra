c4883a3d3b527a7e4f4cff6ed4f0c797a6ea0dc2
Lorenzo603 <lorenzo.furrer@gmail.com>
2016-11-28 15:25:42 +0100

    multiple row selection

--- piovra/src/main/webapp/resources/js/experimentResultWidget.js
+++ piovra/src/main/webapp/resources/js/experimentResultWidget.js
@@ -18,20 +18,10 @@ var ExperimentResultWidget = {
                         paging: false
                     } );
 
-
-
-                    $('input[type="checkbox"]').change(
-                    function() {
-                            var caseIndex = $(this).attr('id').split("-")[2];
-                            if (this.checked) {
-                                $('#tr-case-'+caseIndex).addClass('case-considered');
-                            } else {
-                                $('#tr-case-'+caseIndex).removeClass('case-considered');
-                            }
-                        }
-                    );
-
-
+                    // select rows
+                    $('#casesTable tbody').on( 'click', 'tr', function () {
+                        $(this).toggleClass('active');
+                    } );
 
                 }
             }
